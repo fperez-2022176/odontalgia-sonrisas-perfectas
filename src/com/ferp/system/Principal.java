0b00f902b884ad6ed17414ab8beb43f8540177ee
Marco Barrios <mbarrios-2026491@kinal.edu.gt>
2026-09-09 17:23:59 -0600

    feat: Se agrego el viewfactory y el scenemanager

--- src/com/ferp/system/Principal.java
+++ src/com/ferp/system/Principal.java
@@ -1,6 +1,8 @@
 
 package com.ferp.system;
 
+import com.ferp.system.utils.SceneManager;
+import com.ferp.system.utils.ViewFactory;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -14,7 +16,9 @@ public class Principal extends Application {
 
     @Override
     public void start(Stage escenarioPrincipal) {
-
+    SceneManager.getInstanciaSceneManager().setStagePrincipal(escenarioPrincipal);
+        ViewFactory viewFacto = new ViewFactory();
+        viewFacto.viewLogin();
     }
 
 }
