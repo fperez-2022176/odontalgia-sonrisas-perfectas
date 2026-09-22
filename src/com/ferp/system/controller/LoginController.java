c6a9362ed3202251fc1e3a9954d0247bb203a777
Informatica <informatica@alumno.local>
2026-09-22 14:14:23 -0600

    feat: Se agregaron clases

--- src/com/ferp/system/controller/LoginController.java
+++ src/com/ferp/system/controller/LoginController.java
@@ -1,16 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.ferp.system.controller;
 
-/**
- *
- * @author informatica
- */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
-    
-    
-    
-    
+
+    public void abrirLogin(ActionEvent event) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(
+            getClass().getResource("/com/ferp/system/view/LoginDesarrollador.fxml")
+        );
+
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((javafx.scene.Node) event.getSource())
+                .getScene()
+                .getWindow();
+
+        stage.setScene(new Scene(root));
+
+        stage.setTitle("Login Desarrollador");
+        stage.show();
+    }
 }
+
+
+
+
+    
